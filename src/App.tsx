5480fa0ced2b9f81990202865588edf2a4f4d350
Philip Terber <philipterber350@gmail.com>
2025-07-11 14:05:13 +0200

    Refactor Vite configuration and enhance App component for model loading
    - Updated Vite configuration to streamline asset copying, including the addition of editor styles and model resources.
    - Enhanced App component to load the model from a specified path and improved error handling for model loading.
    - Introduced a raw tokenizer JSON configuration for better text processing capabilities.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import CodeEditor from './components/CodeEditor'; // Importieren der CodeEditor-
 import LessonContent from './components/LessonContent'; // Importieren der LessonContent-Komponente
 import { lessons } from './lessons/index'; // Importieren der Lektionen
 import * as tf from '@tensorflow/tfjs';
+<<<<<<< HEAD
+=======
+
+const rawTokenizerJson = {"class_name": "Tokenizer", "config": {"num_words": 1000, "filters": "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n", "lower": true, "split": " ", "char_level": false, "oov_token": "<OOV>", "document_count": 21, "word_counts": "{\"name\": 11, \"'\": 20, \"print\": 22, \"alter\": 8, \"zahl\": 29, \"f'\": 1, \"ist\": 5, \"jahre\": 1, \"alt'\": 1, \"int\": 4, \"input\": 5, \"'wie\": 3, \"alt\": 2, \"bist\": 3, \"du\": 4, \"zahl1\": 6, \"'gib\": 2, \"die\": 2, \"erste\": 1, \"ein'\": 2, \"zahl2\": 6, \"zweite\": 1, \"hei\\u00dft\": 1, \"f'hallo\": 1, \"farben\": 14, \"'rot'\": 6, \"'gr\\u00fcn'\": 7, \"'blau'\": 6, \"0\": 3, \"append\": 1, \"'gelb'\": 2, \"for\": 2, \"in\": 2, \"range\": 2, \"1\": 3, \"6\": 1, \"while\": 1, \"10\": 4, \"if\": 4, \"3\": 1, \"break\": 1, \"5\": 1, \"'zahl\": 4, \"gr\\u00f6\\u00dfer\": 2, \"als\": 3, \"5'\": 1, \"10'\": 3, \"elif\": 1, \"kleiner\": 1, \"and\": 1, \"zwischen\": 1, \"und\": 1, \"ff0000'\": 3, \"00ff00'\": 3, \"0000ff'\": 3, \"update\": 1, \"ffff00'\": 1, \"def\": 3, \"mein\": 2, \"return\": 3, \"addiere\": 2, \"multipliziere\": 2}", "word_docs": "{\"'\": 8, \"name\": 4, \"print\": 21, \"zahl\": 11, \"alter\": 4, \"f'\": 1, \"jahre\": 1, \"alt'\": 1, \"ist\": 4, \"alt\": 2, \"'wie\": 2, \"bist\": 2, \"du\": 2, \"input\": 3, \"int\": 3, \"zahl2\": 3, \"die\": 1, \"erste\": 1, \"ein'\": 1, \"zweite\": 1, \"'gib\": 1, \"zahl1\": 3, \"f'hallo\": 1, \"hei\\u00dft\": 1, \"'gr\\u00fcn'\": 6, \"'rot'\": 6, \"'blau'\": 6, \"farben\": 6, \"0\": 2, \"'gelb'\": 2, \"append\": 1, \"for\": 2, \"1\": 2, \"in\": 2, \"6\": 1, \"range\": 2, \"while\": 1, \"10\": 3, \"break\": 1, \"3\": 1, \"if\": 4, \"gr\\u00f6\\u00dfer\": 2, \"5'\": 1, \"als\": 2, \"5\": 1, \"'zahl\": 3, \"kleiner\": 1, \"elif\": 1, \"10'\": 2, \"und\": 1, \"and\": 1, \"zwischen\": 1, \"ff0000'\": 3, \"00ff00'\": 3, \"0000ff'\": 3, \"update\": 1, \"ffff00'\": 1, \"return\": 3, \"mein\": 1, \"def\": 3, \"addiere\": 1, \"multipliziere\": 1}", "index_docs": "{\"4\": 8, \"6\": 4, \"3\": 21, \"2\": 11, \"7\": 4, \"43\": 1, \"44\": 1, \"45\": 1, \"13\": 4, \"31\": 2, \"20\": 2, \"21\": 2, \"16\": 2, \"14\": 3, \"15\": 3, \"10\": 3, \"33\": 1, \"46\": 1, \"34\": 1, \"47\": 1, \"32\": 1, \"9\": 3, \"49\": 1, \"48\": 1, \"8\": 6, \"11\": 6, \"12\": 6, \"5\": 6, \"22\": 2, \"35\": 2, \"50\": 1, \"36\": 2, \"23\": 2, \"37\": 2, \"51\": 1, \"38\": 2, \"52\": 1, \"17\": 3, \"54\": 1, \"53\": 1, \"18\": 4, \"39\": 2, \"56\": 1, \"24\": 2, \"55\": 1, \"19\": 3, \"58\": 1, \"57\": 1, \"25\": 2, \"61\": 1, \"59\": 1, \"60\": 1, \"26\": 3, \"27\": 3, \"28\": 3, \"62\": 1, \"63\": 1, \"30\": 3, \"40\": 1, \"29\": 3, \"41\": 1, \"42\": 1}", "index_word": "{\"1\": \"<OOV>\", \"2\": \"zahl\", \"3\": \"print\", \"4\": \"'\", \"5\": \"farben\", \"6\": \"name\", \"7\": \"alter\", \"8\": \"'gr\\u00fcn'\", \"9\": \"zahl1\", \"10\": \"zahl2\", \"11\": \"'rot'\", \"12\": \"'blau'\", \"13\": \"ist\", \"14\": \"input\", \"15\": \"int\", \"16\": \"du\", \"17\": \"10\", \"18\": \"if\", \"19\": \"'zahl\", \"20\": \"'wie\", \"21\": \"bist\", \"22\": \"0\", \"23\": \"1\", \"24\": \"als\", \"25\": \"10'\", \"26\": \"ff0000'\", \"27\": \"00ff00'\", \"28\": \"0000ff'\", \"29\": \"def\", \"30\": \"return\", \"31\": \"alt\", \"32\": \"'gib\", \"33\": \"die\", \"34\": \"ein'\", \"35\": \"'gelb'\", \"36\": \"for\", \"37\": \"in\", \"38\": \"range\", \"39\": \"gr\\u00f6\\u00dfer\", \"40\": \"mein\", \"41\": \"addiere\", \"42\": \"multipliziere\", \"43\": \"f'\", \"44\": \"jahre\", \"45\": \"alt'\", \"46\": \"erste\", \"47\": \"zweite\", \"48\": \"hei\\u00dft\", \"49\": \"f'hallo\", \"50\": \"append\", \"51\": \"6\", \"52\": \"while\", \"53\": \"3\", \"54\": \"break\", \"55\": \"5\", \"56\": \"5'\", \"57\": \"elif\", \"58\": \"kleiner\", \"59\": \"and\", \"60\": \"zwischen\", \"61\": \"und\", \"62\": \"update\", \"63\": \"ffff00'\"}", "word_index": "{\"<OOV>\": 1, \"zahl\": 2, \"print\": 3, \"'\": 4, \"farben\": 5, \"name\": 6, \"alter\": 7, \"'gr\\u00fcn'\": 8, \"zahl1\": 9, \"zahl2\": 10, \"'rot'\": 11, \"'blau'\": 12, \"ist\": 13, \"input\": 14, \"int\": 15, \"du\": 16, \"10\": 17, \"if\": 18, \"'zahl\": 19, \"'wie\": 20, \"bist\": 21, \"0\": 22, \"1\": 23, \"als\": 24, \"10'\": 25, \"ff0000'\": 26, \"00ff00'\": 27, \"0000ff'\": 28, \"def\": 29, \"return\": 30, \"alt\": 31, \"'gib\": 32, \"die\": 33, \"ein'\": 34, \"'gelb'\": 35, \"for\": 36, \"in\": 37, \"range\": 38, \"gr\\u00f6\\u00dfer\": 39, \"mein\": 40, \"addiere\": 41, \"multipliziere\": 42, \"f'\": 43, \"jahre\": 44, \"alt'\": 45, \"erste\": 46, \"zweite\": 47, \"hei\\u00dft\": 48, \"f'hallo\": 49, \"append\": 50, \"6\": 51, \"while\": 52, \"3\": 53, \"break\": 54, \"5\": 55, \"5'\": 56, \"elif\": 57, \"kleiner\": 58, \"and\": 59, \"zwischen\": 60, \"und\": 61, \"update\": 62, \"ffff00'\": 63}"}};
+>>>>>>> 9d3ae4ab67af35f3e41a14b2ada924d634b3a236
 
 type TokenizerJson = {
   word_index: { [word: string]: number };
@@ -79,6 +84,7 @@ const App: React.FC = () => {
   useEffect(() => {
     const loadModel = async () => {
       try {
+<<<<<<< HEAD
         // Use offline path from preload.js
         // @ts-ignore
         const modelPath = window.offlinePaths?.modelJson;
@@ -88,6 +94,10 @@ const App: React.FC = () => {
         } else {
           console.error("Model path not found in offlinePaths.");
         }
+=======
+        modelRef.current = await tf.loadLayersModel('model_web/model.json');
+        console.log("Modell erfolgreich geladen.");
+>>>>>>> 9d3ae4ab67af35f3e41a14b2ada924d634b3a236
       } catch (error) {
         console.error("Fehler beim Laden des Modells:", error);
       }
@@ -273,6 +283,7 @@ except Exception as e:
           setCompletedLessons([...completedLessons, selectedSubLesson]);
         }
         setIsError(false);
+        setOutput("Das Ergebnis ist korrekt!\n\nAusgabe:\n" + output.trim());
       } else {
         setIsError(true);
         setOutput(
